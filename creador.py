3a2b4a1f4a36f332129f9a37be072bcf4928ff6d
Paolo Landeros <landerospaolo@gmail.com>
2023-10-12 14:14:11 -0300

    first commit

---
+++ creador.py
@@ -0,0 +1,10 @@
+# Crear un programa que haga 2 preguntas y las respuestas sean el nombre de la cerveza
+
+print("El nombre de la cerveza será: " 
+    + 
+    (input('¿Qué palabra describe el sabor, la experiencia o el carácter de la cerveza?: '))
+    +
+    ' '
+    +
+    (input('¿Qué palabra está relacionada con el estilo, la historia o el región de la cerveza?: '))
+    )
